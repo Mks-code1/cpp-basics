68fe4e811c2fff16e4215b60391cdf2e727fbe64
MKS <mohmed384@icloud.com>
2026-06-30 15:40:31 +0300

    Add Variables.cpp with basic variable examples

---
+++ Variables.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std; 
+
+
+int main() {
+//1 
+	 
+	string My_name = "Mohamed Kaser Saadalden.", city = "Homs.", country = "Syria."; 
+	int age = 20; 
+	float monthly_salary = 5000;
+	char gender = 'M';
+	bool isMarried = true; 
+
+
+	cout << "**************************************************" << endl;
+	cout << "Name: " << My_name << endl << "Age: " << age << endl; 
+	cout << "City: " << city << endl << "Country: " << endl; 
+	cout << "Monthly Salary: " << monthly_salary << endl << "yearly_salary: " << monthly_salary * 12 << endl; 
+	cout << "Gender: " << gender << endl << "Married: " << isMarried << endl; 
+	cout << "**************************************************" << endl; 
+
+//2 
+	int x = 20, y = 30, b = 10;
+	cout << x << "+" << endl << y << "+" << endl << b << endl << "__________________________________" << endl << "Total: " << x + y + b << endl;
+
+
+//3 
+	int age1 = 25; 
+	cout << endl <<  "You'r age after 5 years will be " << age1 + 5 << " years old." << endl; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0; 
+
+}
